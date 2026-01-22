832e7f86d6f566933a0464f6da547ab3d84dbc52
Chittem Vasanth Kumar <chittemvasanthkumar205@gmail.com>
2026-01-22 19:58:17 +0530

    Add files via upload

---
+++ Control_system_analysis.m
@@ -0,0 +1,40 @@
+clc;
+clear;
+close all;
+
+s = tf('s');
+
+% First order system
+
+G1 = 1/(s + 1);
+
+figure;
+step(G1);
+title('Step Response of First Order System');
+grid on;
+
+% Second order system
+
+G2 = 10/(s^2 + 2*s + 10);
+
+figure;
+step(G2);
+title('Step Response of Second Order System');
+grid on;
+
+H = feedback(G2, 1);
+
+figure;
+step(H);
+title('Closed Loop Step Response of second order system');
+grid on;
+
+figure;
+bode(G2);
+grid on;
+title('Bode Plot of the second order System');
+
+figure;
+rlocus(G2);
+grid on;
+title('Root locus of second order system')
